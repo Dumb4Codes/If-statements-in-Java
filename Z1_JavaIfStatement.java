c67958faf47a9047a074f220c21cfd9aaacd3424
Youvraj Singh <114943098+Dumb4Codes@users.noreply.github.com>
2022-10-13 11:08:19 +0530

    Add files via upload

---
+++ Z1_JavaIfStatement.java
@@ -0,0 +1,25 @@
+
+public class Z1_JavaIfStatement {
+
+	public static void main(String[] args) {
+		// if statement = Performs a block of code if it's condition evaluates to be true
+		
+		
+				// double ==, >= are called comparison operators single ' = ' is for assigning value
+				int age = 21;
+				if (age<=20) {
+					System.out.println("you're good man!");
+				}
+				else if (age<=13) {
+					System.out.println("You're a teenager!");
+				}
+				else if (age>=21) {
+					System.out.println("Hahahahaha Idiot");
+				}
+				else {
+					System.out.println("Damn you're an idiot!");
+				}
+
+	}
+
+}
